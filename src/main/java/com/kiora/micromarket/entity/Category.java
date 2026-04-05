0303909f6a482c349129efc50282a50156b62be1
Aylin Morales <aylin.morales2401@gmail.com>
2026-04-05 16:22:06 -0500

    refactor: update Category entity and add CategoryRequestDTO

--- src/main/java/com/kiora/micromarket/entity/Category.java
+++ src/main/java/com/kiora/micromarket/entity/Category.java
@@ -1,10 +1,15 @@
 package com.kiora.micromarket.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 import java.util.List;
 
 @Entity
 @Table(name = "categories")
+@Getter
+@Setter
 public class Category {
 
     @Id
@@ -15,7 +20,6 @@ public class Category {
     private String description;
 
     @OneToMany(mappedBy = "category")
+    @JsonIgnore // <-- ESTA LÍNEA EVITA EL ERROR 500
     private List<Product> products;
-
-    // Getters & Setters
 }
