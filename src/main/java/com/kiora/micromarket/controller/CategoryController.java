fc765783df374bf820a63f91ec98dcb2c9e72b92
Aylin Morales <aylin.morales2401@gmail.com>
2026-04-05 16:23:18 -0500

    feat: update controllers to support new provider and category operations

--- src/main/java/com/kiora/micromarket/controller/CategoryController.java
+++ src/main/java/com/kiora/micromarket/controller/CategoryController.java
@@ -1,7 +1,11 @@
 package com.kiora.micromarket.controller;
 
+import com.kiora.micromarket.dto.request.CategoryRequestDTO;
 import com.kiora.micromarket.entity.Category;
 import com.kiora.micromarket.service.CategoryService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -17,8 +21,14 @@ public class CategoryController {
     }
 
     @PostMapping
-    public Category create(@RequestBody Category category) {
-        return service.save(category);
+    public ResponseEntity<Category> create(@Valid @RequestBody CategoryRequestDTO categoryDTO) {
+        // Mapeo manual del DTO a la Entidad
+        Category category = new Category();
+        category.setName(categoryDTO.getName());
+        category.setDescription(categoryDTO.getDescription());
+        
+        // Guardamos y devolvemos un 201 Created
+        return ResponseEntity.status(HttpStatus.CREATED).body(service.save(category));
     }
 
     @GetMapping
